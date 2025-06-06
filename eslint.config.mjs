b9fda062346fd987551d15df7dfa79763d25035c
Robert Gogete <gogeterobert@yahoo.com>
2025-06-06 15:50:05 +0300

    Added eslint and prettier to be able to properly lint the solution
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      '**/test/*',
+      '**/dist/*',
+      '**/coverage/*',
+      'tsconfig.json',
+    ]
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs
+    },
+    rules: {
+      ...eslintPluginTs.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    }
+  }
+];
