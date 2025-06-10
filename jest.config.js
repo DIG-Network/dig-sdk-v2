1a68c449bad7fcf4b9bfd5f38628801ba180dfef
Robert Gogete <gogeterobert@yahoo.com>
2025-06-10 11:59:34 +0300

    Added github action to lint and test the code (#1)
    * Added github action to lint and test the code
    * Added tool to comment the coverage status on the PR
    ---------
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,20 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageDirectory: "coverage",
+  testMatch: ["<rootDir>/test/**/*.test.ts"],
 };
