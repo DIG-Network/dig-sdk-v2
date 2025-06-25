ca21a15db769d80dcae43993bc742472c901d88e
Robert Gogete <gogeterobert@yahoo.com>
2025-06-25 08:43:22 +0300

    Cold wallet and added wallet interface (#6)
    * Added config for dig and moved what will not be changedable as constants to the files their are in scope for
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Added default for dig path and updated test
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Added interface for the wallet, new cold wallet object and tests. Need to standardize naming between public key and address
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Made the typing for lastHeight more restrictive
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Added methods to work with the master public key in the cold wallet
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Fixed imports
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    ---------
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

---
+++ src/application/types/ColdWallet.ts
@@ -0,0 +1,56 @@
+import { Peer, UnspentCoinsResponse, addressToPuzzleHash, verifySignedMessage, masterPublicKeyToWalletSyntheticKey, masterPublicKeyToFirstPuzzleHash } from '@dignetwork/datalayer-driver';
+
+export interface IColdWallet {
+  getPuzzleHash(address: string): Buffer;
+  verifyKeySignature(signature: Buffer, publicKey: Buffer, message: Buffer): boolean;
+  listUnspentCoins(
+    peer: Peer,
+    puzzleHash: Buffer,
+    previousHeight: number | undefined | null,
+    previousHeaderHash: Buffer
+  ): Promise<UnspentCoinsResponse>;
+  isCoinSpendable(
+    peer: Peer,
+    coinId: Buffer,
+    lastHeight: number | undefined | null,
+    headerHash: Buffer
+  ): Promise<boolean>;
+  masterPublicKeyToWalletSyntheticKey(publicKey: Buffer): Buffer;
+  masterPublicKeyToFirstPuzzleHash(publicKey: Buffer): Buffer;
+}
+
+export class ColdWallet implements IColdWallet {
+  getPuzzleHash(address: string): Buffer {
+    return addressToPuzzleHash(address);
+  }
+
+  verifyKeySignature(signature: Buffer, publicKey: Buffer, message: Buffer): boolean {
+    return verifySignedMessage(signature, publicKey, message);
+  }
+
+  async listUnspentCoins(
+    peer: Peer,
+    puzzleHash: Buffer,
+    previousHeight: number,
+    previousHeaderHash: Buffer
+  ) {
+    return await peer.getAllUnspentCoins(puzzleHash, previousHeight, previousHeaderHash);
+  }
+
+  async isCoinSpendable(
+    peer: Peer,
+    coinId: Buffer,
+    lastHeight: number,
+    headerHash: Buffer
+  ): Promise<boolean> {
+    return !(await peer.isCoinSpent(coinId, lastHeight, headerHash));
+  }
+
+  masterPublicKeyToWalletSyntheticKey(publicKey: Buffer): Buffer {
+    return masterPublicKeyToWalletSyntheticKey(publicKey);
+  }
+
+  masterPublicKeyToFirstPuzzleHash(publicKey: Buffer): Buffer {
+    return masterPublicKeyToFirstPuzzleHash(publicKey);
+  }
+}
--- src/application/types/Wallet.ts
+++ src/application/types/Wallet.ts
@@ -16,7 +16,25 @@ import { FileCacheService } from '../services/FileCacheService';
 
 const COIN_CACHE_DURATION = 600000;
 
-export class Wallet {
+export interface IWallet {
+  getMnemonic(): string;
+  getMasterSecretKey(): Promise<Buffer>;
+  getPublicSyntheticKey(): Promise<Buffer>;
+  getPrivateSyntheticKey(): Promise<Buffer>;
+  getOwnerPuzzleHash(): Promise<Buffer>;
+  getOwnerPublicKey(): Promise<string>;
+  createKeyOwnershipSignature(nonce: string): Promise<string>;
+  selectUnspentCoins(
+    peer: Peer,
+    coinAmount: bigint,
+    feeBigInt: bigint,
+    omitCoins?: Coin[],
+    lastHeight?: number,
+    lastHeaderHash?: string,
+  ): Promise<Coin[]>;
+}
+
+export class Wallet implements IWallet {
   private mnemonic: string;
 
   public constructor(mnemonic: string) {
@@ -54,7 +72,7 @@ export class Wallet {
 
   public async getOwnerPublicKey(): Promise<string> {
     const ownerPuzzleHash = await this.getOwnerPuzzleHash();
-    return puzzleHashToAddress(ownerPuzzleHash, 'xch');
+    return puzzleHashToAddress(ownerPuzzleHash, 'xch'); // might need to also add txch for testnet
   }
 
   public async createKeyOwnershipSignature(nonce: string): Promise<string> {
