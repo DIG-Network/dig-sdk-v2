27e8f958ac883bbeb4fbd795fe264fc06a8c0fd7
Robert Gogete <gogeterobert@yahoo.com>
2025-07-07 11:20:13 +0300

    Updated the example to show the wallet balance
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

--- test/L1PeerServiceWithIndexer.example.ts
+++ test/L1PeerServiceWithIndexer.example.ts
@@ -4,21 +4,15 @@ import { BlockChainType } from '../src/application/types/BlockChain';
 import { WalletService } from '../src/application/services/WalletService';
 import { CoinRepository } from '../src/infrastructure/Repositories/CoinRepository';
 import config from '../src/config';
+import { Wallet } from '../src/application/types/Wallet';
 
 async function main() {
-  const testnetWalletAddress = "dev";
-  const testnetMnemonic = ""; // Replace with your actual mnemonic
-  const walletDbPath = "wallet.sqlite";
-  const coinRepository = new CoinRepository();
+  const testnetWalletAddress = 'dev';
+  const testnetMnemonic =
+    ''; // Replace with your actual mnemonic
   // You must have ca.crt and ca.key in your working directory or adjust the path
-  const tls = new Tls('ca.crt', 'ca.key');
   config.BLOCKCHAIN_NETWORK = 'testnet';
   try {
-    console.log('Initial coins in db:');
-    coinRepository.getAllCoins().forEach((coin) => {
-      console.log(`Coin ID: ${coin.coinId.toString('hex')}, Amount: ${coin.amount}, Status: ${coin.status}, Wallet ID: ${coin.walletId}`);;
-    });
-
     const coinIndexer = new CoinIndexer();
 
     // This starts the CoinIndexer worker and connects to the testnet chia blockchain
@@ -26,24 +20,22 @@ async function main() {
 
     const wallets = WalletService.getAddresses();
 
-    let wallet;
-    if (!wallets.map((wallet => wallet.name)).includes(testnetWalletAddress)) {
-        wallet = await WalletService.createAddress(testnetWalletAddress, testnetMnemonic);
-        console.log(`Wallet ${testnetWalletAddress} added to DB and keyring.`);
+    let wallet: Wallet;
+    if (!wallets.map((wallet) => wallet.name).includes(testnetWalletAddress)) {
+      wallet = await WalletService.createAddress(testnetWalletAddress, testnetMnemonic);
+      console.log(`Wallet ${testnetWalletAddress} added to DB and keyring.`);
     } else {
-        wallet = await WalletService.createAddress(testnetWalletAddress);
-        console.log(`Wallet ${testnetWalletAddress} loading existing.`);
+      wallet = await WalletService.createAddress(testnetWalletAddress);
+      console.log(`Wallet ${testnetWalletAddress} loading existing.`);
     }
 
     // do a while ininitely that waits for one second each time
     while (true) {
       console.log('-------------------------------------------------');
-      console.log(`Fetching coins from repository at the time ${new Date()}:`);
-      coinRepository.getAllCoins().forEach((coin) => {
-        console.log(`Coin ID: ${coin.coinId.toString('hex')}, Amount: ${coin.amount}, Status: ${coin.status}, Wallet ID: ${coin.walletId}`);
-      });
+      let balance = await wallet.getBalance('xch');
+      console.log(`Balance for wallet ${testnetWalletAddress}: ${balance.balance} ${balance.assetId}`);
       console.log('-------------------------------------------------');
-      await new Promise(resolve => setTimeout(resolve, 10000));
+      await new Promise((resolve) => setTimeout(resolve, 10000));
     }
   } catch (e) {
     console.error('Error during execution:', e);
