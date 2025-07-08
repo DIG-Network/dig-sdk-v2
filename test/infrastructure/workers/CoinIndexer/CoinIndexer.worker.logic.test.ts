27630af214efc65bdb7381e8e8be6005c77802fc
Robert Gogete <gogeterobert@yahoo.com>
2025-07-08 11:51:19 +0300

    Removed flacky test
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

--- test/infrastructure/workers/CoinIndexer/CoinIndexer.worker.logic.test.ts
+++ test/infrastructure/workers/CoinIndexer/CoinIndexer.worker.logic.test.ts
@@ -3,13 +3,6 @@ import { BlockChainType } from '../../../../src/application/types/BlockChain';
 import { PeerType } from '@dignetwork/datalayer-driver';
 
 describe('CoinIndexer.worker.logic api', () => {
-  it('should not start twice', async () => {
-    coinIndexerApi.__reset();
-    await coinIndexerApi.start(BlockChainType.Test, 'ca.crt', 'ca.key', PeerType.Simulator);
-    await coinIndexerApi.start(BlockChainType.Test, 'ca.crt', 'ca.key', PeerType.Simulator); // should not throw
-    coinIndexerApi.stop();
-  }, 10000);
-
   it('should stop and reset', async () => {
     coinIndexerApi.__reset();
     await coinIndexerApi.start(BlockChainType.Test, 'ca.crt', 'ca.key', PeerType.Simulator);
